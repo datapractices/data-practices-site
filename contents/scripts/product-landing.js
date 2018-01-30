2139e8cc3f3fff4456e0f9756f19e10df294c42f
Mayank Shekhar <mayank.shekhar@joshtechnologygroup.com>
2018-01-30 16:02:43 +0530

    Responsive Changes

--- contents/scripts/product-landing.js
+++ contents/scripts/product-landing.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-    $('body').scrollspy({ target: '#navigation' });
+    // $('body').scrollspy({ target: '#navigation' });
+
+    $(window).on('resize', function () {
+        console.log('resize called');
+        var win = $(this);
+        var offsetHeight = $('.navigation').height();
+        console.log('resize called', offsetHeight);
+        if (win.width() < 768) {
+            $('body').scrollspy({
+                target: '#navigation',
+                offset: offsetHeight
+            });
+        }
+    });
+    var offsetHeight = $('.navigation').height();
+    if($(window).width() < 768) {
+        $('body').scrollspy({
+            target: '#navigation',
+            offset: offsetHeight
+        });
+    } else {
+        $('body').scrollspy({
+            target: '#navigation'
+        });
+    }
 });
