a1b50ffe1128cb93ae4b31ca74ca63c783f0a470
Rahul Mittal <rahul.mittal@joshtechnologygroup.com>
2018-01-31 15:18:52 +0530

    Product sign congratulations page and cross browser fixes

--- contents/scripts/main.js
+++ contents/scripts/main.js
@@ -10,17 +10,25 @@ $(document).ready(function() {
 
 function alignItems(elements) {
     if(window.innerWidth > 768 ) {
-        elements.split(',').forEach(function(element) {
-            var maxHeight = 0;
-            element = $(element);
-            element.each(function() {
-                var elemHeight = $(this).height();
-                if(elemHeight > maxHeight) {
-                    maxHeight = elemHeight;
-                }
-            });
+        setTimeout(function() {
+            elements.split(',').forEach(function(element) {
+                var maxHeight = 0;
+                element = $(element);
+                element.height('');
+                element.each(function() {
+                    var elemHeight = $(this).height();
+                    if(elemHeight > maxHeight) {
+                        maxHeight = elemHeight;
+                    }
+                });
 
-            element.height(maxHeight);
-        });
+                element.height(maxHeight);
+            });
+        }, 100);
     }
+}
+
+function getQueryParameter(parameter) {
+    var half = location.search.split(parameter + '=')[1];
+    return half !== undefined ? decodeURIComponent(half.split('&')[0]) : null;
 }
