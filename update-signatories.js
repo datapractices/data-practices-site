58facf208446db9cc685b68f43f8d46964af6571
Eric Capps <eric.capps@data.world>
2018-02-02 14:52:46 -0600

    Fetch code of ethics signatories

--- update-signatories.js
+++ update-signatories.js
@@ -2,7 +2,9 @@ const axios = require('axios')
 const csvjson = require('csvjson')
 const fs = require('fs')
 
-const manifestSignatoriesQueryUrl = 'https://query.data.world/s/9sBOkOomWru8CeO19vNmzN1wNrhRDY'
+const manifestSignatoriesQueryUrl = 'https://query.data.world/s/koBtctk-Sl4RXvfm7fC9vDkBIZpSwR'
+const codeOfEthicsSignatoriesQueryUrl = 'https://query.data.world/s/eUBdjjpbXwzwAG_tRekxhnjxHEcy5r'
+
 axios.get(manifestSignatoriesQueryUrl).then(
   response => {
     // parse cs
@@ -10,4 +12,13 @@ axios.get(manifestSignatoriesQueryUrl).then(
     fs.writeFileSync('./contents/manifesto/signatories.json', JSON.stringify(manifestoSignatories))
   },
   error => console.error('Failed to write approved manifesto signatories: ', error)
+)
+
+axios.get(codeOfEthicsSignatoriesQueryUrl).then(
+  response => {
+    // parse cs
+    const codeOfEthicsSignatories = csvjson.toObject(response.data)
+    fs.writeFileSync('./contents/code-of-ethics/signatories.json', JSON.stringify(codeOfEthicsSignatories))
+  },
+  error => console.error('Failed to write approved code of ethics signatories: ', error)
 )
