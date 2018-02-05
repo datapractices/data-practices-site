acbce2de62ac4a274da46040754aff74e64dfcff
Mark DiMarco <mark.dimarco@gmail.com>
2018-02-05 17:47:34 -0600

    social links

--- contents/scripts/product-landing.js
+++ contents/scripts/product-landing.js
@@ -43,4 +43,19 @@ $(document).ready(function() {
             $socialHandleWrap.find('input[type="hidden"]').val('');
         }
     });
+
+    $('.social-link.facebook').on('click', function(e) {
+      e.preventDefault()
+      window.open('http://www.facebook.com/sharer/sharer.php?u=datapractices.org', '_blank', 'top=0,left=500,width=600,height=400')
+    })
+
+    $('.social-link.twitter').on('click', function(e) {
+      e.preventDefault()
+      window.open('http://twitter.com/home?status=I%20just%20signed%20the%20Data%20Practices%20Manifesto,%20which%20describes%20the%20most%20effective,%20ethical,%20and%20modern%20approach%20to%20data%20teamwork.%20Join%20the%20movement%20at%20datapractices.org', '_blank', 'top=0,left=500,width=600,height=400')
+    })
+
+    $('.social-link.twitter-ethics').on('click', function(e) {
+      e.preventDefault()
+      window.open('http://twitter.com/home?status=The%20CPEDS%20(Community%20Principles%20for%20Ethical%20Data%20Sharing)%20movement%20is%20underway!%20Join%20the%20effort%20at%20datapractices.org', '_blank', 'top=0,left=500,width=600,height=400')
+    })
 });
