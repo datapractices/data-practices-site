7faaac536e0e2214c07f4f22e529681e4f7e94fe
Mark DiMarco <mark.dimarco@gmail.com>
2018-02-05 18:20:09 -0600

    update twitter

--- contents/scripts/product-landing.js
+++ contents/scripts/product-landing.js
@@ -51,11 +51,11 @@ $(document).ready(function() {
 
     $('.social-link.twitter').on('click', function(e) {
       e.preventDefault()
-      window.open('http://twitter.com/home?status=I%20just%20signed%20the%20Data%20Practices%20Manifesto,%20which%20describes%20the%20most%20effective,%20ethical,%20and%20modern%20approach%20to%20data%20teamwork.%20Join%20the%20movement%20at%20datapractices.org', '_blank', 'top=0,left=500,width=600,height=400')
+      window.open('http://twitter.com/home?status=I%20just%20signed%20the%20Data%20Practices%20Manifesto,%20which%20describes%20the%20most%20effective,%20ethical,%20and%20modern%20approach%20to%20data%20teamwork.%20Join%20the%20movement%20at%20https%3A%2F%2Fhubs.ly%2FH09W81F0', '_blank', 'top=0,left=500,width=600,height=400')
     })
 
     $('.social-link.twitter-ethics').on('click', function(e) {
       e.preventDefault()
-      window.open('http://twitter.com/home?status=The%20CPEDS%20(Community%20Principles%20for%20Ethical%20Data%20Sharing)%20movement%20is%20underway!%20Join%20the%20effort%20at%20datapractices.org', '_blank', 'top=0,left=500,width=600,height=400')
+      window.open('http://twitter.com/home?status=The%20CPEDS%20(Community%20Principles%20for%20Ethical%20Data%20Sharing)%20movement%20is%20underway!%20Join%20the%20effort%20at%20https%3A%2F%2Fhubs.ly%2FH09W81F0', '_blank', 'top=0,left=500,width=600,height=400')
     })
 });
