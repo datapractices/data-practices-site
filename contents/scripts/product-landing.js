3ecd9a08df623aee24e5a553af27d416d782d6c1
Joe Boutros <jboutros@gmail.com>
2018-02-06 00:16:12 -0600

    fix sharing links and live stream link

--- contents/scripts/product-landing.js
+++ contents/scripts/product-landing.js
@@ -64,19 +64,19 @@ $(document).ready(function() {
 
     $('.social-link.twitter').on('click', function(e) {
       e.preventDefault();
-      window.open('https://twitter.com/home?status=The%20Data%20Practices%20Manifesto%20and%20Community%20Principles%20for%20Ethical%20Data%20Sharing%20https%3A%2F%2Fhubs.ly%2FH09W81F0', '_blank', 'top=0,left=500,width=600,height=400');
+      window.open('https://twitter.com/home?status=The%20Data%20Practices%20Manifesto%20and%20Community%20Principles%20for%20Ethical%20Data%20Sharing%20https%3A%2F%2Fdatapractices.org', '_blank', 'top=0,left=500,width=600,height=400');
       return false
     });
 
     $('.social-link.twitter-manifesto').on('click', function(e) {
         e.preventDefault();
-        window.open('https://twitter.com/home?status=I%20just%20signed%20the%20Data%20Practices%20Manifesto,%20which%20describes%20the%20most%20effective,%20ethical,%20and%20modern%20approach%20to%20data%20teamwork.%20Join%20the%20movement%20at%20https%3A%2F%2Fhubs.ly%2FH09W81F0', '_blank', 'top=0,left=500,width=600,height=400');
+        window.open('https://twitter.com/home?status=I%20just%20signed%20the%20Data%20Practices%20Manifesto,%20which%20describes%20the%20most%20effective,%20ethical,%20and%20modern%20approach%20to%20data%20teamwork.%20Join%20the%20movement%20at%20https%3A%2F%2Fdatapractices.org%2Fmanifesto%2F', '_blank', 'top=0,left=500,width=600,height=400');
         return false
     });
 
     $('.social-link.twitter-ethics').on('click', function(e) {
       e.preventDefault();
-      window.open('https://twitter.com/home?status=The%20CPEDS%20(Community%20Principles%20for%20Ethical%20Data%20Sharing)%20movement%20is%20underway!%20Join%20the%20effort%20at%20https%3A%2F%2Fhubs.ly%2FH09W81F0', '_blank', 'top=0,left=500,width=600,height=400');
+      window.open('https://twitter.com/home?status=The%20CPEDS%20(Community%20Principles%20for%20Ethical%20Data%20Sharing)%20movement%20is%20underway!%20Join%20the%20effort%20at%20https%3A%2F%2Fdatapractices.org%2Fcommunity-principles-on-ethical-data-sharing%2F', '_blank', 'top=0,left=500,width=600,height=400');
       return false
     });
 });
